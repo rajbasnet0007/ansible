3e7117c58f4b16d165f2760b083061bd438db940
Raj Kumar <raj.kumar@grazitti.com>
2020-07-29 16:35:01 +0530

    shell script

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p /src/raj
+df -h > /tmp/space.txt
+echo "raj" >> /tmp/space.txt
+mv /etc/nginx/sites-available/default /etc/nginx/sites-available/bkp
+mv ssl/test.crt /etc/nginx/sites-available
+systemctl restart nginx
