e235a63f7966f6dcaf55a2df1998728db7422d1b
rajbasnet0007 <36179005+rajbasnet0007@users.noreply.github.com>
2020-07-30 16:58:36 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 mkdir -p /src/raj
 df -h > /tmp/space.txt
 echo "raj" >> /tmp/space.txt
-mv /etc/nginx/sites-available/default /etc/nginx/sites-available/bkp
+##mv /etc/nginx/sites-available/default /etc/nginx/sites-available/bkp
 mv ssl/test.crt /etc/nginx/sites-available
 systemctl restart nginx
