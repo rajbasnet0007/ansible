c5fbb8bcc00bc87ea4be020da99bb26a960f101a
Raj Kumar <raj.kumar@grazitti.com>
2020-12-28 13:15:59 +0530

    nginx-docker

--- test/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-systemctl stop apt-daily.timer
-systemctl disable apt-daily.timer
-systemctl disable apt-daily.service
-systemctl stop apt-daily-upgrade.timer
-systemctl disable apt-daily-upgrade.timer
-systemctl disable apt-daily-upgrade.service
